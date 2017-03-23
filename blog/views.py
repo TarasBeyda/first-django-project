56602cb3436052c29b7e61d8fec6f245ca864358
TarasBeyda <tarasbeyda@gmail.com>
2017-03-23 22:47:57 +0200

    allowed-hosts

--- blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
